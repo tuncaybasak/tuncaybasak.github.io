606f111c3c52124d198a35a6d148fdd2f23e5217
tuncaybasak <tuncaybasak@gmail.com>
2021-01-06 21:31:24 +0300

    Create linkData.js

---
+++ linkData.js
@@ -0,0 +1,35 @@
+var linkData = 
+[
+    {        
+        "category": "Online Tools",        
+        "links": 
+            [
+                {
+                    "name": "codepen",
+                    "src": "https://codepen.io/",   
+                },
+                {
+                    "name": "jsfiddle",
+                    "src": "https://jsfiddle.net/",   
+                },
+                {
+                    "name": "jsbin",
+                    "src": "https://jsbin.com/",   
+                }
+            ]
+    },
+    {
+        "category": "Design Tools",  
+        "links": 
+            [
+                {
+                    "name": "google fonts",
+                    "src": "https://fonts.google.com",   
+                },
+                {
+                    "name": "fontawesome",
+                    "src": "https://fontawesome.com/",   
+                },              
+            ]
+    }
+];
