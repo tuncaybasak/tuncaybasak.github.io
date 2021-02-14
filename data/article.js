363bd0f02bdd0526c56870bfc4f24c4b15fd2565
Tuncay Başak <tuncaybasak@outlook.com>
2021-02-14 19:42:43 +0300

    geliştirme

---
+++ data/article.js
@@ -0,0 +1,60 @@
+var articleData = [
+    {        
+        "category": "Database",
+        "sequence": 1,        
+        "links": 
+            [
+                { "sequence": 1, "name": "13 BEST Free Database", "src": "https://www.guru99.com/free-database-software.html" },                           
+                { "sequence": 2, "name": "SQL Performans Arttırma Önerileri", "src": "https://www.cozumpark.com/yazilim-gelistiriciler-icin-sql-performans-arttirma-onerileri/" },                           
+            ]
+    },
+    {        
+        "category": "Medium",
+        "sequence": 4,        
+        "links": 
+            [
+                { "sequence": 1, "name": "hepsiburadatech", "src": "https://medium.com/hepsiburadatech" },       
+                { "sequence": 2, "name": "Hepsiburada Micro Frontends Dönüşümü", "src": "https://medium.com/hepsiburadatech/hepsiburada-micro-frontend-d%C3%B6n%C3%BC%C5%9F%C3%BCm%C3%BC-4c2f26b8dcae" },       
+                { "sequence": 3, "name": "abisteknoloji", "src": "https://medium.com/abisteknoloji" },       
+                { "sequence": 4, "name": "NET Framework’ten .NET Core‘a Göç Hikayemiz", "src": "https://medium.com/abisteknoloji/net-frameworkten-net-core-a-g%C3%B6%C3%A7-abe9c76e450" },       
+                { "sequence": 5, "name": "git sunucu servisleri", "src": "https://medium.com/bambam-t%C3%BCrkiye/git-sunucu-servisleri-alternatifler-556269acbc3c" },       
+            ]
+    },
+    {        
+        "category": "javascript Libraries",
+        "sequence": 2,        
+        "links": 
+            [
+                { "sequence": 1, "name": "TypeScript For Beginners", "src": "https://levelup.gitconnected.com/typescript-for-beginners-97b568d3e110" },                            
+                { "sequence": 2, "name": "youmightnotneedjquery", "src": "http://youmightnotneedjquery.com/" },                            
+            ]
+    },   
+    {        
+        "category": "React",
+        "sequence": 3,        
+        "links": 
+            [
+                { "sequence": 1, "name": "ReactJS Bölüm 1", "src": "https://medium.com/kodcular/reactjs-b%C3%B6l%C3%BCm-1-79ea7541344f" },                            
+                { "sequence": 2, "name": "React with the Fetch API and axios", "src": "https://css-tricks.com/using-data-in-react-with-the-fetch-api-and-axios/" },                            
+                { "sequence": 3, "name": "React lazy loading libraries", "src": "https://blog.logrocket.com/the-top-choices-for-react-lazy-loading-libraries-in-2021/" },                            
+                { "sequence": 4, "name": "publish-subscribe pattern", "src": "https://itnext.io/why-every-beginner-front-end-developer-should-know-publish-subscribe-pattern-72a12cd68d44" },                            
+            ]
+    },   
+    {        
+        "category": "webapi",
+        "sequence": 5,        
+        "links": 
+            [                                        
+                { "sequence": 1, "name": "Web API Tarafında Dapper Kullanımı", "src": "https://www.buraksenyurt.com/post/web-api-tarafinda-dapper-kullanimi" },                            
+            ]
+    },   
+    {        
+        "category": "Sqlite",
+        "sequence": 6,        
+        "links": 
+            [                                        
+                { "sequence": 1, "name": "simple CRUD using SQLite in C#", "src": "https://riptutorial.com/csharp/example/17513/creating-simple-crud-using-sqlite-in-csharp" },                            
+            ]
+    }, 
+];
+    
--- data/site.js
+++ data/site.js
@@ -19,7 +19,8 @@ var siteData = [
             [
                 { "sequence": 2, "name": "entityframeworktutorial", "src": "https://www.entityframeworktutorial.net/" },
                 { "sequence": 1, "name": "net-informations", "src": "http://net-informations.com/" },
-                { "sequence": 3, "name": "csharp-examples", "src": "https://www.csharp-examples.net/"}                         
+                { "sequence": 3, "name": "csharp-examples", "src": "https://www.csharp-examples.net/"},                         
+                { "sequence": 4, "name": "newtonsoft    ", "src": "https://www.newtonsoft.com/json"}                         
             ]
     },
     {        
@@ -38,6 +39,48 @@ var siteData = [
                 { "sequence": 1, "name": "fontawesome", "src": "https://fontawesome.com/" },                                 
                 { "sequence": 2, "name": "google fonts", "src": "https://fonts.google.com/" },                                 
                 { "sequence": 3, "name": "pixabay", "src": "https://pixabay.com/"}                                 
+            ]
+    },
+    {        
+        "category": "Online Tools",
+        "sequence": 5,        
+        "links": 
+            [
+                { "sequence": 1, "name": "codepen", "src": "https://codepen.io/" },                                 
+                { "sequence": 2, "name": "jsfiddle", "src": "https://jsfiddle.net/" },                                 
+                { "sequence": 3, "name": "jsbin", "src": "https://jsbin.com/"},                                 
+                { "sequence": 4, "name": "json formatter", "src": "https://jsonformatter.curiousconcept.com/"},                                 
+                { "sequence": 5, "name": "char counter", "src": "https://charcounter.com/en/"},                                 
+            ]
+    },
+    {        
+        "category": "Bootstart",
+        "sequence": 6,        
+        "links": 
+            [
+                { "sequence": 1, "name": "bootstrap", "src": "https://getbootstrap.com/" },                                 
+                { "sequence": 2, "name": "bootadmin", "src": "https://bootadmin.net/" },                                    
+                { "sequence": 3, "name": "bootsnipp", "src": "https://bootsnipp.com/" },                                    
+                { "sequence": 4, "name": "startbootstrap", "src": "https://startbootstrap.com/" },                                    
+                { "sequence": 5, "name": "bootstrapzero", "src": "https://www.bootstrapzero.com/c/bootstrap-admin-themes" },                                    
+                { "sequence": 6, "name": "coreui", "src": "https://coreui.io/" },                                    
+            ]
+    },
+    {        
+        "category": "cheatsheet",
+        "sequence": 7,        
+        "links": 
+            [
+                { "sequence": 1, "name": "React.js  ", "src": "https://devhints.io/react" },                                                                              
+            ]
+    },
+    {        
+        "category": "api",
+        "sequence": 8,        
+        "links": 
+            [
+                { "sequence": 1, "name": "jsonplaceholder", "src": "https://jsonplaceholder.typicode.com/guide/" },                                                                              
+                { "sequence": 2, "name": "jsonapi", "src": "https://jsonapi.org/examples/" },                                                                              
             ]
     }
 ];
